d908dbc24608fca58927036b8296e39efa5bb3cf
Emanuel <emanuel.sjovall@gmail.com>
2025-04-11 09:36:10 +0200

    Update telescope to not be pined to speciffic branch, updates

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
     'nvim-telescope/telescope.nvim',
     enabled = true,
     event = 'VimEnter',
-    branch = '0.1.x',
+    -- branch = '0.1.x',
     dependencies = {
         'nvim-lua/plenary.nvim',
         { -- If encountering errors, see telescope-fzf-native README for install instructions
